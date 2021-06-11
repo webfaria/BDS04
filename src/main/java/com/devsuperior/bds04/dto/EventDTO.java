a7aff1fd3649752b7f8c82324c8a2943c8b1fd49
Cleber <webfaria@gmail.com>
2021-06-11 07:30:58 -0300

    First commit

--- src/main/java/com/devsuperior/bds04/dto/EventDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/EventDTO.java
@@ -3,15 +3,25 @@ package com.devsuperior.bds04.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.devsuperior.bds04.entities.Event;
 
 public class EventDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	
+	@FutureOrPresent(message = "A Data não pode ser anterior")
 	private LocalDate date;
 	private String url;
+	
+	@NotNull(message = "Campo requerido")
 	private Long cityId;
 	
 	public EventDTO() {
